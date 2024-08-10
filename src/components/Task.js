0c755311099863f385f5c8757a8ea93d6b2f0934
annemusau <moigeanne646@gmail.com>
2024-08-10 10:35:20 +0800

    commiting all

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React from 'react';
 
-function Task() {
+function Task({ task, onDeleteTask }) {
+  if (!task) {
+    return null;
+  }
+  
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div>{task.text}</div>
+      <div>{task.category}</div>
+      <button onClick={() => onDeleteTask(task.id)}>Delete</button>
     </div>
   );
 }
